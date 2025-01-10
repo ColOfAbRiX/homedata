44ded87e7a3c89448712edea40c64fc55f27dc9a
Fabrizio Colonna <colofabrix@tin.it>
2025-01-10 08:35:45 +0000

    Done more atomicity but not sure if tado service is not working

--- homedata/src/main/scala/com/colofabrix/scala/homedata/Main.scala
+++ homedata/src/main/scala/com/colofabrix/scala/homedata/Main.scala
@@ -14,7 +14,7 @@ object Main extends IOApp.Simple:
   val (from, to) =
     TimeSpanPicker()
       .selectFrom()
-      .otherMinus(days = 2)
+      .otherMinus(years = 1)
       .roundBoth(ChronoUnit.DAYS)
       .pick()
 
@@ -26,5 +26,5 @@ object Main extends IOApp.Simple:
       tadoPuller         <- TadoPuller()
       tadoMeasures        = tadoPuller.pullReadings(from, to).through(toApiMeasureStream)
       writer             <- InfluxWriter()
-      _                  <- writer.write(gasMeasures, electricityMeasures, tadoMeasures)
+      _                  <- writer.write(tadoMeasures)
     yield ()
